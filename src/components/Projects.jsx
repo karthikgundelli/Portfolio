4963c67d18b05542fe1e1c254bd24eb655cae239
Karthik Gundelly <karthikgundelly67@gmail.com>
2025-10-22 14:22:39 +0530

    Include project links in Projects component
    Added project links to each project entry for easier access.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,18 +6,21 @@ const Projects = () => {
     {
        title:"FurniFlex – E-commerce Furniture Store",
       description: "FurniFlex is a fully responsive, modern e-commerce furniture store built with React.js.It allows users to browse products, filter by category, search by name, add items to cart, and manage cart quantities.The project also features a light/dark theme toggle, a mobile-friendly navbar with hamburger menu, and a professional UI/UX with animations and responsive design.This project demonstrates strong React fundamentals, state management using Context API, and front-end development skills, making it resume-ready for job applications" ,
-       tech:"React.js, React Router DOM, Context API"
+       tech:"React.js, React Router DOM, Context API",
+       link: "https://furni-flex-com.vercel.app/",
       
     },
     {
       title: "EstateMitra – Real Estate Platform",
       description: "Developed a real estate web app enabling users to buy and sell land without intermediaries. Implemented user authentication and database storage using Firebase. Designed responsive UI components enhancing usability across all devices. Deployed on Vercel with optimized performance.",
       tech: "React.js, Firebase, Bootstrap, Vercel",
+      link: "https://estate-mitra.vercel.app/",
     },
     {
       title: "GreenNest – Organic Grocery E-commerce App",
       description: "Built an online grocery platform inspired by Blinkit with a modern and clean UI. Integrated real-time database for product listing and cart management using Firebase. Optimized loading performance and responsiveness for mobile-first experience. Deployed seamlessly using Vercel ensuring scalability.",
       tech: "React.js, Firebase, Bootstrap",
+      link:"https://green-nest-kappa.vercel.app/login",
     },
   ];
 
@@ -29,6 +32,7 @@ const Projects = () => {
           <div key={index} className="project-card">
             <h3>{proj.title}</h3>
             <p>{proj.description}</p>
+            <a href={proj.link}>click here to preview</a>
             <small><strong>Tech Stack:</strong> {proj.tech}</small>
           </div>
         ))}
@@ -38,5 +42,6 @@ const Projects = () => {
 };
 
 export default Projects;
+
 
 
